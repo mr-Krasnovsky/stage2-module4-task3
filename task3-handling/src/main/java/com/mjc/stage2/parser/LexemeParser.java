1a2dc5fce06fbff74a625aac7e0375ffc7cb9e20
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-06-27 16:02:31 +0300

    fixed some bugs

--- task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
@@ -1,6 +1,7 @@
 package com.mjc.stage2.parser;
 
 import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.SymbolLeaf;
 import com.mjc.stage2.entity.TextComponent;
 import com.mjc.stage2.entity.TextComponentType;
 
@@ -12,7 +13,7 @@ public class LexemeParser extends AbstractTextParser{
     public void parse(AbstractTextComponent abstractTextComponent, String string) {
         char [] array = string.toCharArray();
         for (char ch: array){
-            abstractTextComponent.add(new TextComponent(TextComponentType.WORD));
+            abstractTextComponent.add(new SymbolLeaf(ch, TextComponentType.WORD));
         }
     }
 }
